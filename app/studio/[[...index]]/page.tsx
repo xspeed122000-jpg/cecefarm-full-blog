ac9fc7fe79c8cb561d4c320763235f770c193298
unknown <xspeed122000@gmail.com>
2026-04-19 22:14:46 +0700

    Add edge runtime config for studio

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -4,6 +4,8 @@ import { NextStudio } from 'next-sanity/studio'
 // ここで sanity.config.ts を読み込みます。パス（../）の数はフォルダの深さに合わせます。
 import config from '../../../sanity.config'
 
+export const runtime = 'edge';
+
 export default function StudioPage() {
   return (
     <div style={{ height: '100vh', width: '100%' }}>
