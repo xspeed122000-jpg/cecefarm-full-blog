7361e7ac5e76c1a02bfc40024d5e143bec72f304
unknown <xspeed122000@gmail.com>
2026-04-28 16:57:13 +0700

    change permanent false

--- next.config.ts
+++ next.config.ts
@@ -7,19 +7,19 @@ const nextConfig: NextConfig = {
       {
         source: '/phyto_cites',
         destination: '/jp/phyto_cites',
-        permanent: true,
+        permanent: false,
       },
       // 2. 旧ブログURLからの転送
       {
         source: '/posts/:slug',
         destination: '/items/:slug',
-        permanent: true,
+        permanent: false,
       },
       // 3. ドメイン直下のアクセスのみを転送（★最後の .+ を [^/]+ に変更しました）
       {
         source: '/:slug((?!$|about|pizza|service|shop|contact|items|studio|api|privacy-policy|jp|en|th|_next|static|favicon\\.ico|.*\\.[a-zA-Z0-9]+)[^/]+)',
         destination: '/items/:slug',
-        permanent: true,
+        permanent: false,
       },
     ];
   },
