c80aeca40a92e0a554a72bd9a226f733faf2b8bc
unknown <xspeed122000@gmail.com>
2026-05-04 14:15:46 +0700

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,13 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Inter, Playfair_Display } from 'next/font/google';
 import type { Metadata } from "next";
-
-const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' });
 const playfair = Playfair_Display({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-playfair'// ★後で簡単に呼び出せるように名前をつけます
 });
+const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' });
+
 
 export const metadata: Metadata = {
   robots: {
