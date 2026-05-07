f290ea3a9ee71b0a58596a02d09ed1cf487ca05d
unknown <xspeed122000@gmail.com>
2026-05-07 10:44:56 +0700

    add redirects

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,26 @@ const nextConfig: NextConfig = {
         destination: '/items/:slug',
         permanent: false,
       },
+      {
+        source: '/th', // 旧URL
+        destination: '/', // 新しい対応ページ（とりあえずトップなど）
+        permanent: true, // 301リダイレクト（評価を引き継ぐ設定）
+      },
+      {
+        source: '/items/category/plants',
+        destination: '/items',
+        permanent: true,
+      },
+      {
+        source: '/contact-us', // 旧URL
+        destination: '/contact', // 新しい対応ページ（とりあえずトップなど）
+        permanent: true, // 301リダイレクト（評価を引き継ぐ設定）
+      },
+      {
+        source: '/items/page/',
+        destination: '/items',
+        permanent: true,
+      },
     ];
   },
 };
