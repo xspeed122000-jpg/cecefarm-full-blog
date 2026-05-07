831031dc1624998403813583c3bcbf02e4dea8ba
unknown <xspeed122000@gmail.com>
2026-05-07 19:17:16 +0700

    Unified to Lang

--- app/pizza/page.tsx
+++ app/pizza/page.tsx
@@ -25,7 +25,7 @@ export default function PizzaPage() {
         We are currently preparing to deliver the best pizza to you.<br />
         Please wait a little longer until our opening.<br />
         現在、最高のピザをお届けするための準備を進めております。<br />
-        オープンまでもうしばらくお待ちください。
+        オープンまで、もうしばらくお待ちください。
       </p>
     </main>
   );
