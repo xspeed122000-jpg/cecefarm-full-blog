b189d4e6ce6bfc24d868690ea8a459b582309a1d
unknown <xspeed122000@gmail.com>
2026-05-14 21:10:17 +0700

    fix footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,9 +39,9 @@ export default function Footer() {
                 <div style={{ flex: '1', minWidth: '150px' }}>
                     <h3 style={{ fontSize: '0.9rem', color: '#555555' }}>Support</h3>
                     <nav style={{ display: 'flex', flexDirection: 'column', gap: '10px', fontSize: '0.85rem' }}>
-                        <Link href={`/${lang}/Sitemap`} style={{ color: '#7f7c7c', textDecoration: 'none' }}>Sitemap</Link>
-                        <Link href={`/${lang}/Privacy Policy`} style={{ color: '#7f7c7c', textDecoration: 'none' }}>Privacy Policy</Link>
-                        <Link href={`/${lang}/Contact`} style={{ color: '#7f7c7c', textDecoration: 'none' }}>Contact</Link>
+                        <Link href={`/${lang}/sitemap`} style={{ color: '#7f7c7c', textDecoration: 'none' }}>sitemap</Link>
+                        <Link href={`/${lang}/privacy-policy`} style={{ color: '#7f7c7c', textDecoration: 'none' }}>privacy-policy</Link>
+                        <Link href={`/${lang}/contact`} style={{ color: '#7f7c7c', textDecoration: 'none' }}>contact</Link>
                     </nav>
                 </div>
             </div>
